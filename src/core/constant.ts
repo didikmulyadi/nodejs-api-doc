5e0a936ff61fc7257fe77d03dc2b182bbaccb618
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-14 22:52:05 +0700

    chore: add default title

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -2,5 +2,6 @@ import { UIDocType } from './web'
 
 const DEFAULT_UI: UIDocType = 'stoplight'
 const DEFAULT_PATH: string = '/docs'
+const DEFAULT_TITLE: string = 'API Docs'
 
-export { DEFAULT_PATH, DEFAULT_UI }
+export { DEFAULT_PATH, DEFAULT_UI, DEFAULT_TITLE }
