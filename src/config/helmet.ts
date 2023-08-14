2c656d2be0ef867f38211d59b7676c8a3f257477
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-14 15:11:58 +0700

    chore: add config for helmet library

---
+++ src/config/helmet.ts
@@ -0,0 +1,61 @@
+/**
+ * Allow a request to below domain
+ */
+const nodeApiDocHelmetOptionScripts: string[] = [
+  "'self'",
+  "'unsafe-inline'",
+  'https://unpkg.com',
+  'https://stackpath.bootstrapcdn.com',
+  'https://cdnjs.cloudflare.com',
+  'https://unpkg.com/@stoplight',
+  'https://code.jquery.com',
+  'https://cdnjs.cloudflare.com',
+  'https://stackpath.bootstrapcdn.com',
+  'https://cdn.jsdelivr.net',
+  'https://cdn.redoc.ly',
+]
+
+/**
+ * Allow a request to below domain
+ */
+const nodeApiDocHelmetOptionStyles: string[] = [
+  "'self'",
+  "'unsafe-inline'",
+  'https://unpkg.com',
+  'https://stackpath.bootstrapcdn.com',
+  'https://cdnjs.cloudflare.com',
+  'https://fonts.googleapis.com',
+]
+
+/**
+ * Allow a request to below path/domain
+ */
+const nodeApiDocHelmetOptionWorkers: string[] = [
+  "'self'",
+  "'unsafe-inline'",
+  'blob:',
+]
+
+/**
+ * If you are using a helmet library, there may be a problem with CSP, use this to solve it.
+ * If you have already an option, and want to merge this config, you can use NestApiDocHelmetOptionScripts and NestApiDocHelmetOptionStyles
+ */
+const nodeApiDocHelmetOption: any = {
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: nodeApiDocHelmetOptionScripts,
+      styleSrc: nodeApiDocHelmetOptionStyles,
+      workerSrc: nodeApiDocHelmetOptionWorkers,
+    },
+  },
+  crossOriginResourcePolicy: { policy: 'cross-origin' },
+  crossOriginEmbedderPolicy: false,
+}
+
+export {
+  nodeApiDocHelmetOptionScripts,
+  nodeApiDocHelmetOptionStyles,
+  nodeApiDocHelmetOptionWorkers,
+  nodeApiDocHelmetOption,
+}
