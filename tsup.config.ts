4c9af825640c5a9031398464d092064d592575c5
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-14 22:51:52 +0700

    chore: always use production mode

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from "tsup";
-
-const isProduction = process.env.NODE_ENV === "production";
+import { defineConfig } from 'tsup'
 
 export default defineConfig(({ watch = false }) => ({
   clean: true,
   dts: true,
   entry: {
-    index: "src/index.ts",
+    index: 'src/index.ts',
   },
   external: [],
-  format: ["cjs", "esm", "iife"],
-  minify: isProduction,
-  sourcemap: isProduction,
+  format: ['cjs', 'esm', 'iife'],
+  minify: true,
+  sourcemap: 'inline',
   watch,
-}));
+}))
