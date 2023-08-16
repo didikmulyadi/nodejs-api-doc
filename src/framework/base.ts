f719f4aeca3687cca1a3610984a75c826bdc593d
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-16 07:56:09 +0700

    chore: create JSApiDocBaseFramework as a base framework

---
+++ src/framework/base.ts
@@ -0,0 +1,50 @@
+import { DEFAULT_PATH, DEFAULT_TITLE, DEFAULT_UI } from '@/core/constant'
+import { Config, ConfigOnConstructor } from '@/core/type'
+
+class JSApiDocBaseFramework {
+  app: any
+  document: any
+  config: Config
+
+  /**
+   *
+   * @param app App should be the result of NestFactory.create function
+   * @param document This document is the openAPIObject
+   * @param config
+   * @example app
+   * 
+   * // Nest.js with Express
+   * const app = await NestFactory.create(AppModule);
+   * 
+   * // Nest.js with Fastify
+   * const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+     )
+  );
+   * // Express
+   * const app = express()
+   * 
+   */
+  constructor(app: any, document: object, config: ConfigOnConstructor = {}) {
+    const {
+      defaultUI = DEFAULT_UI,
+      customPath = DEFAULT_PATH,
+      title = DEFAULT_TITLE,
+    } = config
+
+    if (!customPath.startsWith('/', 0)) {
+      throw new Error(`Custom path "${customPath}" should be "/${customPath}" `)
+    }
+
+    this.app = app
+    this.document = document
+    this.config = {
+      title,
+      defaultUI,
+      customPath,
+    }
+  }
+}
+
+export { JSApiDocBaseFramework }
