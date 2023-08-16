421f02c8200177814064fa0692388f0c77196b8c
didikmulyadi <didikmulyadi12@gmail.com>
2023-08-16 07:55:29 +0700

    chore: add IBaseFramework as an implementor

--- src/core/type.ts
+++ src/core/type.ts
@@ -24,4 +24,16 @@ type ConfigOnConstructor = {
   customPath?: string
 }
 
-export type { Config, ConfigOnConstructor }
+interface IBaseFramework {
+  /**
+   * Start the docs with specific path
+   *
+   * This start will define 2 path with GET method
+   * 1. /docs/openapi return the open api json
+   * 2. /docs return the api docs UI
+   *
+   */
+  start(): void
+}
+
+export type { Config, ConfigOnConstructor, IBaseFramework }
